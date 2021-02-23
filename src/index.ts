4bc4b1cbbf8fc37e4893438c6adb9c56479211b5
toxicdesire <malivp3494@gmail.com>
2021-02-23 13:30:21 +0530

    Checking out / exploring Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
 import express from "express";
 import {PORT} from './config/constants';
+import {PrismaClient} from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) =>{
-    res.send('Hello, world!');
+    okay(async () =>{
+        await prisma.user.create({
+            data:{
+                username:'alice_wonderland',
+                email:'alice_from_wonderland@prisma.io',
+                password:'bruh',
+                profile:{
+                    create:{name:'Alison Wonderland', bio:'I dont like turtles?'},
+                },
+            },
+        });
+        const allUsers = await prisma.user.findMany({
+            include:{
+                profile:true,
+            }
+        });
+        res.send(JSON.stringify(allUsers));
+    });
 });
 
 app.listen(PORT, ()=>{
     console.log(`Server running at http://localhost:${PORT}`);
 })
+
+let okay = (f) => {
+    f().catch(e => {throw e;}).finally(async () =>{
+        await prisma.$disconnect();
+    })
+}
