f37b1f4ab0d7a6f32da786b3e86c5c0e87d7c581
toxdes <malivp3494@gmail.com>
2021-09-24 22:02:44 +0530

    try - 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
+echo "--- [CLEANING] ---" && \
 rm -rf node_modules dist && \
+echo "--- [FETCHING LATEST CHANGES] ---" && \
 git fetch origin && \
 git checkout main && \
 git merge origin/main && \
 git checkout dev && \
 git merge origin/dev && \
+echo "--- [FETCHING DEPENDENCIES] ---" && \
 yarn && \
-cp sample.env .env && \
-vi .env && \
+echo "--- [EDITING .env file] ---" && \
+# cp sample.env .env && \
+cp ~/secret-env .env && \
+# vi .env && \
+source .env && \
 yarn gen && \
 yarn build && \
 pm2 delete index && \
-pm2 start dist/index.js && \
+pm2 start dist/index.js
