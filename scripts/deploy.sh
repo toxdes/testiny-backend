e00aaa4c764dbee5e785768547aadd9e3b446566
toxdes <malivp3494@gmail.com>
2021-09-24 21:55:37 +0530

    deploy script - try 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-rm -rf node_modules dist
-git fetch origin
-git checkout main
-git merge origin/main
-git checkout dev
-git merge origin/dev
-yarn
-yarn gen
-yarn build
-pm2 delete index
-pm2 start dist/index.js
+rm -rf node_modules dist && \
+git fetch origin && \
+git checkout main && \
+git merge origin/main && \
+git checkout dev && \
+git merge origin/dev && \
+yarn && \
+cp sample.env .env && \
+vi .env && \
+yarn gen && \
+yarn build && \
+pm2 delete index && \
+pm2 start dist/index.js && \
